7640166d34b65eb325b74dc5e324af306e3e0680
“mohammedbenloucif” <mbenlouc@emich.edu>
2023-02-08 18:38:50 -0500

    edit car rental demo

--- pages/rentalDemo.jsx
+++ pages/rentalDemo.jsx
@@ -4,15 +4,13 @@ import Link from 'next/link';
 const rentalDemo = () => {
   return (
     <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center pt-40'>
-        <iframe 
-            src="https://docs.google.com/presentation/d/e/2PACX-1vTjnGgY31YA5xETGZkV-sMRMrOAw1sBGhSSZm-d_n75jsSlD-Y01ylQau-RKELh6Z11CQWbHJegHqnB/embed?start=true&loop=true&delayms=5000" 
-            frameborder="0" 
-            width="960" 
-            height="569" 
-            allowfullscreen="true" 
-            mozallowfullscreen="true" 
-            webkitallowfullscreen="true"
-        >
+        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRBc1NlEGjXTrLqDDtW3PQhgSp9vnmXH_LSQ3N5C-W5cke274dixVmKFH57E-4DQw/embed?start=true&loop=true&delayms=3000" 
+                frameborder="0" 
+                width="960" 
+                height="569" 
+                allowfullscreen="true" 
+                mozallowfullscreen="true" 
+                webkitallowfullscreen="true">
         </iframe>
         <div>
         <Link href='/#projects'>
