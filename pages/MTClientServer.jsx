fdb035b1bc812247d20081be588e29fb79d86829
“mohammedbenloucif” <mbenlouc@emich.edu>
2023-04-07 02:29:38 -0400

    cleaned up UI for MTClientServer project page

--- pages/MTClientServer.jsx
+++ pages/MTClientServer.jsx
@@ -6,10 +6,9 @@ import Link from 'next/link';
 const MTClientServer = () => {
   return (
     <div className='w-full'>
-        <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
-            <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
-            <div className='absolute top-[80%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-                <h2 className='py-2'>Multi-threaded Chat Service</h2>
+        <div className='w-screen h-[30vh] lg:h-[20vh] relative'>
+            <div className='absolute top-[80%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] text-center translate-y-[-50%] z-10 p-2'>
+                <h2 className='py-2'>Secure Multi-threaded Chat Service</h2>
             </div>
         </div>
 
