8b5ed04dce0fa4e75d0c124f8066fd62867cb010
“mohammedbenloucif” <mbenlouc@emich.edu>
2023-04-15 18:26:14 -0400

    update container dimensions for project section

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 const ProjectItem = ({title, technologies, backgroundImg, projectUrl}) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-        <iframe className='rounded-xl group-hover:opacity-10' src={backgroundImg} width='450' height='400' frameborder="0" scrolling="no" seamless alt='/' />
+        <iframe className='rounded-xl group-hover:opacity-10' src={backgroundImg} width='400' height='280' frameborder="0" scrolling="no" seamless alt='/' />
         <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
             <h3 className='text-xl text-white tracking-wider text-center'>{title}</h3>
             <p className='pb-4 pt-2 text-white text-center'>{technologies}</p>
